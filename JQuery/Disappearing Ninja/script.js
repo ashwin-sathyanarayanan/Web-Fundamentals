1fa6bb220de425b547bbdd0d5c7b82332b9c78c8
Ashwin Sathyanarayanan <ashwin.sathyanarayanan@cgius.com>
2021-07-01 18:24:37 -0700

    Git Assignment

--- JQuery/Disappearing Ninja/script.js
+++ JQuery/Disappearing Ninja/script.js
@@ -1,4 +1,8 @@
 $(function(){
+    // $(document).ready(function(){
+    //     for(i=0; i<10; i++){
+    //         $('#pics').append('<img src="harrypotter.png"></img>');
+    // }});
     $('img').click(function() {
         $(this).hide();
     });
